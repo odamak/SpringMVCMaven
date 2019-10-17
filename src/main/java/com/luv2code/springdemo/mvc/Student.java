cad369be253e4f2e55ef36c370f2b70a16a94c61
unknown <omardamak1@gmail.com>
2019-10-17 13:19:24 +0100

    Add dropdown option in class instead of jsp page

--- src/main/java/com/luv2code/springdemo/mvc/Student.java
+++ src/main/java/com/luv2code/springdemo/mvc/Student.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
@@ -8,7 +10,16 @@ public class Student {
 	
 	public Student() {
 		
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		countryOptions.put("TN", "Tunisia");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		
 	}
+	
+	private LinkedHashMap<String, String> countryOptions;
 
 	public String getFirstName() {
 		return firstName;
@@ -33,9 +44,10 @@ public class Student {
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	
-	
-	
-	
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+		
 
 }
